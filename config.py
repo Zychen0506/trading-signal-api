b845e151aacdc669795850ff09132c577686e3a6
Zychen0506 <johnson90208@gmail.com>
2025-06-24 02:28:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_NOTIFY_TOKEN = os.environ.get("LINE_NOTIFY_TOKEN")
 LINE_USER_ID = os.environ.get("LINE_USER_ID")
